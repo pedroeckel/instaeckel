f5757ef600735d0e6b007f528d1df65fe8156548
Pedro Vinicius Eckel Moreira <pedroeckel@gmail.com>
2021-02-17 00:38:21 -0300

    aula 05

--- pages/src/components/commons/Menu/index.js
+++ pages/src/components/commons/Menu/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Logo } from '../../../theme/Logo';
 import { Button } from '../Button';
-import { MenuWrapper } from "./styles/MenuWrapper";
+import { MenuWrapper } from './styles/MenuWrapper'
+
 
 
 
