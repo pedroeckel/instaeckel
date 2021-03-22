145815dbdcb273fb427b5d2e9a70fa6fe1f345d5
pedro <pedroeckel@gmail.com>
2021-03-22 00:42:05 -0300

    feat: adicionado faq dinâmico

--- pages/faq/index.js
+++ pages/faq/index.js
@@ -1,31 +1,39 @@
 import React from 'react';
 import FAQScreen from '../../src/components/screens/FAQScreen';
 
-export default function FAQPage({ faqCategories }) {
-  /* const [faqCategories, setfaqCategories] = React.useState([]);
+export default function FAQPage(props) {
+  // const [faqCategories, setFaqCategories] = React.useState([]);
 
-  React.useEffect(() => {
-    fetch('https://instalura-api.vercel.app/api/content/faq')
-      .then((respostaDoServer) => respostaDoServer.json())
-      .then((respostaConvertida) => respostaConvertida.data)
-      .then((resposta) => {
-        setfaqCategories(resposta);
-      });
-  }, []); */
+  // React.useEffect(() => {
+  //   fetch('https://instalura-api.vercel.app/api/content/faq').then(async (res) => {
+  //     const response = await res.json();
+  //     return response.data;
+  //   })
+  //     .then((faqCategoriesFromServer) => {
+  //       setFaqCategories(faqCategoriesFromServer);
+  //     });
+  // });
+
+  // const props = {
+  //   faqCategories,
+  // };
 
   return (
-    <FAQScreen faqCategories={faqCategories} />
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <FAQScreen {...props} />
   );
 }
 
 export async function getStaticProps() {
-  const faqCategories = await fetch('https://instalura-api.vercel.app/api/content/faq')
-    .then((respostaDoServer) => respostaDoServer.json())
-    .then((respostaConvertida) => respostaConvertida.data);
+  const faqCategories = await fetch('https://instalura-api.vercel.app/api/content/faq').then(async (res) => {
+    const response = await res.json();
+    return response.data;
+  });
 
+  // Falar sobre tamanho da página aqui e tomar cuidado com recursos extras que vão pra página
   return {
     props: {
       faqCategories,
-    }, // will be passed to the page component as props
+    },
   };
 }
