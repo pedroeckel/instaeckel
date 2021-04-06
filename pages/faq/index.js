9055a337536a8bc06d86da76e419bc489b2ec7f1
pedro <pedroeckel@gmail.com>
2021-04-06 11:14:22 -0300

    feat: finalmodulo5

--- pages/faq/index.js
+++ pages/faq/index.js
@@ -1,28 +1,21 @@
 import React from 'react';
 import FAQScreen from '../../src/components/screens/FAQScreen';
+import websitePageHOC from '../../src/components/wrappers/WebsitePage/hoc';
 
-export default function FAQPage(props) {
-  // const [faqCategories, setFaqCategories] = React.useState([]);
-
-  // React.useEffect(() => {
-  //   fetch('https://instalura-api.vercel.app/api/content/faq').then(async (res) => {
-  //     const response = await res.json();
-  //     return response.data;
-  //   })
-  //     .then((faqCategoriesFromServer) => {
-  //       setFaqCategories(faqCategoriesFromServer);
-  //     });
-  // });
-
-  // const props = {
-  //   faqCategories,
-  // };
-
+function FAQPage(props) {
   return (
     // eslint-disable-next-line react/jsx-props-no-spreading
     <FAQScreen {...props} />
   );
 }
+
+export default websitePageHOC(FAQPage, {
+  pageWrapperProps: {
+    seoProps: {
+      headTitle: 'Perguntas Frequentes',
+    },
+  },
+});
 
 export async function getStaticProps() {
   const faqCategories = await fetch('https://instalura-api.vercel.app/api/content/faq').then(async (res) => {
@@ -30,7 +23,6 @@ export async function getStaticProps() {
     return response.data;
   });
 
-  // Falar sobre tamanho da página aqui e tomar cuidado com recursos extras que vão pra página
   return {
     props: {
       faqCategories,
